b8b3c5092026eda86b78c8abf0017c52fd5232a0
Mitu Fikir <hannatefera2@gmail.com>
2019-11-02 22:22:02 -0700

    Added clicky game components

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
+import friends from "./friends.json";
+import Header from "./components/Header";
+
+
+function shuffle(images) {
+  for (let i = images.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [images[i], images[j]] = [images[j], images[i]];
+  }
+  return images;
+}
+
+class App extends Component {
+  state = {
+    friends,
+    score: 0,
+    topScore : 0,
+    ids : []
+  };
+  
+  handleClick = id => {
+    let ids = this.state.ids;
+    let score = this.state.score;
+    let topScore = this.state.topScore;
+
+    if(ids.indexOf(id) === -1){
+      ids.push(id);
+      this.setState({ score: this.state.score + 1 });
+      this.makeShuffle();
+
+      if(score + 1 >= topScore){
+        this.setState({ topScore: this.state.score + 1 });
+      }
+    } else{
+      this.setState({ score: 0 });
+      this.setState({ ids: [] });
+      this.makeShuffle();
+    }
+    
+  };
+
+  makeShuffle = () => {
+    this.setState({ friends: shuffle(friends) });
+  };
+  
+  render() {
+    return (
+      <div>
+        <Header score={this.state.score}
+          topScore={this.state.topScore}
+        />
+        <Wrapper>
+          {this.state.friends.map(friend => (
+            <FriendCard
+              
+              id={friend.id}
+              key={friend.id}
+              
+              image={friend.image}
+              handleClick={this.handleClick}
+            />
+          ))}
+          
+        </Wrapper>
+      <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
