c77404003fd2bc48cea1fcc8739c4d8e58700742
Mitu Fikir <hannatefera2@gmail.com>
2019-11-02 22:53:35 -0700

    Updates

---
+++ precache-manifest.35b18c8e36db43d094a3b00530eca687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32589fbfe6b29799a6ee20d2eae8dd1f",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "0dfcd6be21da3ef2ffcb",
+    "url": "/clicky-game/static/css/main.f486f561.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/clicky-game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "0dfcd6be21da3ef2ffcb",
+    "url": "/clicky-game/static/js/main.dcdabfe9.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
