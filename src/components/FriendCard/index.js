bf0213d7a3065c418eb0cbc8acfb0673f7f4c004
Mitu Fikir <hannatefera2@gmail.com>
2019-12-19 17:35:07 -0800

    Updated card style and added image

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -2,16 +2,12 @@ import React from "react";
 import "./style.css";
 
 function FriendCard(props) {
-  
   return (
     <div className="card">
       <div className="img-container">
         <img alt={props.id} src={props.image} onClick={() => props.handleClick(props.id)} />
       </div>
-      <span onClick={() => props.handleClick(props.id)} className="spin">
-        
-      </span>
-      
+      <span onClick={() => props.handleClick(props.id)} className="spin"></span>
     </div>
   );
 }
